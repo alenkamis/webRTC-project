d889e7ae02f98bc0e4ef2dfd73d37d9b9a3e874a
Kamabey <93948161+Kamabey@users.noreply.github.com>
2022-01-18 13:52:08 +0100

    Add files via upload

---
+++ db/sql/create.sql
@@ -0,0 +1,11 @@
+create database if not exists web;
+use web;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `trn_date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
